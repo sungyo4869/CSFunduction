976501ec698df93cd0b346e2a83a36130a7d92be
sungyo4869 <mi211324@edu.okinawa-ct.ac.jp>
2023-07-24 03:06:02 +0900

    staticについて学んだ

--- Program.cs
+++ Program.cs
@@ -4,21 +4,21 @@
     {
         static void Main(string[] args)
         {
-            var Voice = new Voice();
-            Voice.WakeUp();
-            Voice.WakeUp("hina");
+            var test1 = new Test();
+            test1.a = 3;
+            Console.WriteLine(test1.a); //　>> 3
+            Console.WriteLine(Test.b);  //  >> 6  Testクラスの中のbという指定方法
+
+            var test2 = new Test();
+            Console.WriteLine(test2.a); //  >> 5
         }
     }
-    public class Voice
-    {
-        public void WakeUp()
-        {
-            Console.WriteLine("起きなさい！");
-        }
 
-        public void WakeUp(string name)
-        {
-            Console.WriteLine(name + "起きなさい！");
-        }
+    public class Test
+    {
+        public int a = 5;
+        public static int b = 6;
+
+
     }
 }
