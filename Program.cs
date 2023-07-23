48224d0203d459a0a4e48853767d1e081e8e2473
sungyo4869 <mi211324@edu.okinawa-ct.ac.jp>
2023-07-23 22:45:25 +0900

    キャストとパースについて学んだ

--- Program.cs
+++ Program.cs
@@ -4,3 +4,13 @@ double b = 5.5;
 
 Console.WriteLine(a);
 Console.WriteLine(b);
+
+// キャスト
+long a = 5;
+int b = (int)a;  // int b = a; >> error
+Console.WriteLine(b);
+
+// パース
+string a = "10";
+int b = int.Parse(a);
+Console.WriteLine(b);
