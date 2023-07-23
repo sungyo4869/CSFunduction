890ae98eba3773ba34c89115fce5feff9f660707
sungyo4869 <mi211324@edu.okinawa-ct.ac.jp>
2023-07-23 22:31:00 +0900

    変数について学んだ

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string helloworld = "Hello, World";
+
+Console.WriteLine(helloworld);
+Console.WriteLine(helloworld);
+Console.WriteLine(helloworld);
+Console.WriteLine(helloworld);
