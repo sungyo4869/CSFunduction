b9aa30ca365736ee4940353ef06825131e016b71
sungyo4869 <mi211324@edu.okinawa-ct.ac.jp>
2023-08-05 17:51:54 +0900

    継承の概要について学んだ

--- Program.cs
+++ Program.cs
@@ -1,24 +1,30 @@
 ﻿namespace HelloWorld
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            var test1 = new Test();
-            test1.a = 3;
-            Console.WriteLine(test1.a); //　>> 3
-            Console.WriteLine(Test.b);  //  >> 6  Testクラスの中のbという指定方法
+            var anonatsu = new Anonatsu();
+            anonatsu.hobby = "sleep";
+            anonatsu.Name = "anonatsu";
 
-            var test2 = new Test();
-            Console.WriteLine(test2.a); //  >> 5
+            Console.WriteLine(anonatsu.Name);
+
+            anonatsu.sayHi();
         }
     }
 
-    public class Test
+    public class Person
     {
-        public int a = 5;
-        public static int b = 6;
+        public string Name;
+        public void sayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
 
-
+    public class Anonatsu:Person
+    {
+        public string hobby;
     }
 }
