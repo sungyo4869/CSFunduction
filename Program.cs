96c2c057dc20a4e46043dbed837e3f28a53ae3b6
sungyo4869 <mi211324@edu.okinawa-ct.ac.jp>
2023-08-07 02:22:13 +0900

    インターフェースについて学んだ

--- Program.cs
+++ Program.cs
@@ -4,37 +4,32 @@
     {
         public static void Main(string[] args)
         {
-            var anonatsu = new Anonatsu("anonatsu");
-
+            var anonatsu = new Anonatsu();
             anonatsu.SayHi();
-
-            // var person = new Person();
-            // ↑ は出来ない (抽象クラスだから)
         }
     }
 
-    public abstract class Person
+    public interface IPerson
+    // インターフェースは暗黙の了解的にIを入れる
     {
-        protected string _name;
-
-        public Person(string name)
-        {
-            _name = name;
-        }
-
-        public abstract void SayHi();
-        // メソッド名と戻り値だけ書く
+        public void SayHi();
+    }
+    public interface ICreature
+    {
+        public string Voice();
     }
 
-    public class Anonatsu : Person
+    public class Anonatsu : IPerson, ICreature
+    // 抽象クラスと違って二つ以上継承できる
     {
-        public Anonatsu(string name) : base(name)
+        public void SayHi()
         {
+            Console.WriteLine("I'm anonatsu");
         }
 
-        public override void SayHi()
+        public void Voice()
         {
-            Console.WriteLine("I'm " + _name);
+            
         }
     }
 }
