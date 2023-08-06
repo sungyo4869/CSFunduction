4fa01775b6593f431a88defba75b42336fd3bee1
sungyo4869 <mi211324@edu.okinawa-ct.ac.jp>
2023-08-07 02:38:52 +0900

    listについて学んだ

--- Program.cs
+++ Program.cs
@@ -4,19 +4,21 @@
     {
         public static void Main(string[] args)
         {
-            var anonatsu = new Anonatsu();
-            anonatsu.SayHi();
+            // 二通り書き方がある
+            var list1 = new List<string>();
+
+            list1.Add("a");
+            list1.Add("b");
+            list1.Add("c");
+
+            var list2 = new List<string>
+            {
+                "a", "b", "c"
+            };
+
+            var array = new string[3] {"a", "b", "c"};
+
+            Console.WriteLine(list1[0]);
         }
     }
-
-    public sealed class Person
-    {
-        public string Name;
-    }
-
-    // public class Anonatsu : Person
-    // {
-        
-    // }
-    // sealedがついてるから継承できない
 }
