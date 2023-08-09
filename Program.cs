2d99300194edc2ccdf4560935d1b952f33ccd776
sungyo4869 <mi211324@edu.okinawa-ct.ac.jp>
2023-08-10 00:04:24 +0900

    匿名メソッドについて学んだ

--- Program.cs
+++ Program.cs
@@ -5,19 +5,18 @@
         public delegate void Test(string s);
         public static void Main(string[] args)
         {
-            var test = new Test(TestMethod1);
-            test += new Test(TestMethod2);
+            // var test = new Test(TestMethod1);
+
+            var test = delegate (string s) 
+            {
+                Console.WriteLine(s + "!");
+            };
             test("yes");
         }
 
-        public static void TestMethod1 (string s)
-        {
-            Console.WriteLine(s + "!");
-        }
-
-        public static void TestMethod2 (string s)
-        {
-            Console.Write(s + "!!!");
-        }
+        // public static void TestMethod1 (string s)
+        // {
+        //     Console.WriteLine(s + "!");
+        // }
     }
 }
